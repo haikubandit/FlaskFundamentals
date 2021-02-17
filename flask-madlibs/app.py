48a4f7da28914846b043728bdcee39d0767ed18e
Daniel Knapp <knapparatus@gmail.com>
2021-02-17 13:54:54 -0600

    adding further study features to madlibs app

--- flask-madlibs/app.py
+++ flask-madlibs/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 from random import randint,  choice, sample
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import *
+import stories
 
 app = Flask(__name__)
 
@@ -10,17 +10,52 @@ app.config['SECRET_KEY'] = "a-secret-key"
 debug = DebugToolbarExtension(app)
 
 
-@app.route('/')
-def madlib_form():
-    """Shows home page"""
+
+# @app.route('/')
+# def madlib_form():
+#     """Shows home page"""
+
+#     prompts = story.prompts
+#     return render_template('base.html', prompts=prompts)
+
+
+@app.route("/")
+def ask_story():
+    """Show list-of-stories form."""
+
+    return render_template("base.html", stories=stories.values())
+
+@app.route("/questions")
+def ask_questions():
+    """Generate and show form to ask words."""
+
+    story_id = request.args["story_id"]
+    story = stories[story_id]
+
     prompts = story.prompts
-    return render_template('base.html', prompts=prompts)
 
+    return render_template("questions.html",
+                           story_id=story_id,
+                           title=story.title,
+                           prompts=prompts)
 
-@app.route('/story')
-def story_page():
-    """Displays user story"""
-    story.generate(request.args)
-    your_story = story.template
+# @app.route('/story')
+# def story_page():
+#     """Displays user story"""
 
-    return render_template("story.html", your_story=your_story)
+#     text = story.generate(request.args)
+
+#     return render_template("story.html", text=text)
+
+@app.route("/story")
+def show_story():
+    """Show story result."""
+
+    story_id = request.args["story_id"]
+    story = stories[story_id]
+
+    text = story.generate(request.args)
+
+    return render_template("story.html",
+                           title=story.title,
+                           text=text)
--- flask-madlibs/stories.py
+++ flask-madlibs/stories.py
@@ -18,9 +18,11 @@ class Story:
         'I love to eat a good mango.'
     """
 
-    def __init__(self, words, text):
+    def __init__(self, code, title, words, text):
         """Create story with words and template text."""
 
+        self.code = code
+        self.title = title
         self.prompts = words
         self.template = text
 
@@ -37,9 +39,15 @@ class Story:
 
 # Here's a story to get you started
 
-
-story = Story(
+story1 = Story(
     ["place", "noun", "verb", "adjective", "plural_noun"],
-    """Once upon a time in a long-ago {place}, there lived a
-       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
+    """Once upon a time in a long-ago {place}, there lived a large {adjective} {noun}. It loved to {verb} {plural_noun}.""")
+
+
+story2 = Story(
+    ["name", "adjective", "occupation", "noun"],
+    """Once upon a time in a galaxy far, far away, {name}, a {adjective} {occupation} discovered a new {noun}. """
 )
+
+# Make dict of {code:story, code:story, ...}
+stories = {s.code: s for s in [story1, story2]}
